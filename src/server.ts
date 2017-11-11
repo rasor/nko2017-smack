00a40448c3548bc08eee474f61cea9813998ec2a
Søren Raarup <soren@rasor.dk>
2017-11-11 07:15:11 +0100

    try to get flash to work

--- src/server.ts
+++ src/server.ts
@@ -87,7 +87,7 @@ app.use(expressValidator());
 // }));
 // app.use(passport.initialize());
 // app.use(passport.session());
-// app.use(flash());
+app.use(flash());
 // app.use(lusca.xframe("SAMEORIGIN"));
 // app.use(lusca.xssProtection(true));
 // app.use((req, res, next) => {
