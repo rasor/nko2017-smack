4e8092a76754c0b05ba360dd800d3dd38de64f28
Søren Raarup <soren@rasor.dk>
2017-11-12 16:31:05 +0100

    graphObjs.IFacebookAPIUserResponse

--- src/controllers/api.ts
+++ src/controllers/api.ts
@@ -3,6 +3,7 @@
 import * as async from "async";
 import * as request from "request";
 import * as graph from "fbgraph";
+import * as graphObjs from "facebook";
 import { Response, Request, NextFunction } from "express";
 
 
@@ -23,7 +24,7 @@ export let getApi = (req: Request, res: Response) => {
 export let getFacebook = (req: Request, res: Response, next: NextFunction) => {
   const token = req.user.tokens.find((token: any) => token.kind === "facebook");
   graph.setAccessToken(token.accessToken);
-  graph.get(`${req.user.facebook}?fields=id,name,email,first_name,last_name,gender,link,locale,timezone`, (err: Error, results: graph.FacebookUser) => {
+  graph.get(`${req.user.facebook}?fields=id,name,email,first_name,last_name,gender,link,locale,timezone`, (err: Error, results: graphObjs.IFacebookAPIUserResponse) => {
     if (err) { return next(err); }
     res.render("api/facebook", {
       title: "Facebook API",
@@ -40,13 +41,27 @@ export let getFbResp = (req: Request, res: Response, next: NextFunction) => {
   const token = req.user.tokens.find((token: any) => token.kind === "facebook");
   graph.setAccessToken(token.accessToken);
   const reqMe = `${req.user.facebook}?fields=id,name,location,friends,gender`;
-  graph.get(reqMe, (err: Error, respMe: graph.FacebookUser) => {
+  graph.get(reqMe, (err: Error, respMe: graphObjs.IFacebookAPIUserResponse) => {
     if (err) { return next(err); }
+
+    const respFriends: graphObjs.IFacebookAPIUserResponse[] = [];
+    // any friends use this app?
+    // if (respMe.friends && respMe.friends.data.length > 0) {
+    //   respMe.friends.data.forEach(f => {
+    //     const reqFr = `${f.id}?location`;
+    //     graph.get(reqFr, (err: Error, respFr: graphObjs.IFacebookAPIUserResponse) => {
+    //       if (err) { return next(err); }
+
+    //       respFriends.push(respFr);
+    //     });
+    //   });
+    // }
 
     res.render("api/facebook/fbresp", {
       title: "Facebook Data",
       reqMe: reqMe,
-      respMe: respMe
+      respMe: respMe,
+      respFriends: respFriends
     });
   });
 };
